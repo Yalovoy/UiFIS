4c945df263cbf772d11eed7d354cbc9edeb9dba4
Yalovoy <160518237+Yalovoy@users.noreply.github.com>
2026-02-13 00:11:31 +0300

    Add files via upload

---
+++ Practicum/PR3/MotionAnalyzer/MotionAnalyzer/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace MotionAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.txtAcceleration = new System.Windows.Forms.TextBox();
+            this.txtV0 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtFinalSpeed = new System.Windows.Forms.TextBox();
+            this.txtDistance = new System.Windows.Forms.TextBox();
+            this.txtMotionType = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lblEquation = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(56, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(442, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Анализатор механического движения";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.btnCalculate);
+            this.groupBox1.Controls.Add(this.txtTime);
+            this.groupBox1.Controls.Add(this.txtAcceleration);
+            this.groupBox1.Controls.Add(this.txtV0);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 62);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(387, 170);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Входные данные";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(202, 125);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(102, 39);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(22, 125);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(102, 39);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Рассчитать";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(173, 100);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(168, 22);
+            this.txtTime.TabIndex = 5;
+            // 
+            // txtAcceleration
+            // 
+            this.txtAcceleration.Location = new System.Drawing.Point(173, 67);
+            this.txtAcceleration.Name = "txtAcceleration";
+            this.txtAcceleration.Size = new System.Drawing.Size(168, 22);
+            this.txtAcceleration.TabIndex = 4;
+            // 
+            // txtV0
+            // 
+            this.txtV0.Location = new System.Drawing.Point(173, 31);
+            this.txtV0.Name = "txtV0";
+            this.txtV0.Size = new System.Drawing.Size(168, 22);
+            this.txtV0.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Время движения:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Ускорение:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Начальная скорость:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtFinalSpeed);
+            this.groupBox2.Controls.Add(this.txtDistance);
+            this.groupBox2.Controls.Add(this.txtMotionType);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(12, 238);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(516, 164);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Результат";
+            // 
+            // txtFinalSpeed
+            // 
+            this.txtFinalSpeed.Location = new System.Drawing.Point(159, 109);
+            this.txtFinalSpeed.Name = "txtFinalSpeed";
+            this.txtFinalSpeed.ReadOnly = true;
+            this.txtFinalSpeed.Size = new System.Drawing.Size(168, 22);
+            this.txtFinalSpeed.TabIndex = 5;
+            // 
+            // txtDistance
+            // 
+            this.txtDistance.Location = new System.Drawing.Point(159, 77);
+            this.txtDistance.Name = "txtDistance";
+            this.txtDistance.ReadOnly = true;
+            this.txtDistance.Size = new System.Drawing.Size(168, 22);
+            this.txtDistance.TabIndex = 4;
+            // 
+            // txtMotionType
+            // 
+            this.txtMotionType.Location = new System.Drawing.Point(158, 49);
+            this.txtMotionType.Name = "txtMotionType";
+            this.txtMotionType.ReadOnly = true;
+            this.txtMotionType.Size = new System.Drawing.Size(339, 22);
+            this.txtMotionType.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(10, 112);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(135, 16);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Конечная скорость:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 83);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 16);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Пройденный путь:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 16);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Тип движения:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.chart1);
+            this.groupBox3.Controls.Add(this.lblEquation);
+            this.groupBox3.Location = new System.Drawing.Point(593, 22);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(481, 380);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "График зависимости";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(21, 40);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(454, 300);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // lblEquation
+            // 
+            this.lblEquation.AutoSize = true;
+            this.lblEquation.Location = new System.Drawing.Point(18, 350);
+            this.lblEquation.Name = "lblEquation";
+            this.lblEquation.Size = new System.Drawing.Size(122, 16);
+            this.lblEquation.TabIndex = 0;
+            this.lblEquation.Text = "Уравнение: S(t)=...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1102, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.TextBox txtAcceleration;
+        private System.Windows.Forms.TextBox txtV0;
+        private System.Windows.Forms.TextBox txtFinalSpeed;
+        private System.Windows.Forms.TextBox txtDistance;
+        private System.Windows.Forms.TextBox txtMotionType;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblEquation;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Practicum/PR3/MotionAnalyzer/MotionAnalyzer/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace MotionAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeChart();
+        }
+
+        private void InitializeChart()
+        {
+            chart1.Series.Clear();
+            chart1.ChartAreas.Clear();
+
+            ChartArea chartArea = new ChartArea("MotionChart");
+            chartArea.AxisX.Title = "Время, t (с)";
+            chartArea.AxisY.Title = "Путь, S (м)";
+            chartArea.AxisX.Minimum = 0;
+            chartArea.AxisY.Minimum = 0;
+            chartArea.BackColor = Color.White;
+
+            chart1.ChartAreas.Add(chartArea);
+
+            Series lineSeries = new Series("Path")
+            {
+                ChartType = SeriesChartType.Line,
+                Color = Color.Blue,
+                BorderWidth = 2,
+                MarkerStyle = MarkerStyle.None
+            };
+            chart1.Series.Add(lineSeries);
+
+            Series pointSeries = new Series("KeyPoints")
+            {
+                ChartType = SeriesChartType.Point,
+                Color = Color.Red,
+                MarkerSize = 8,
+                MarkerStyle = MarkerStyle.Circle
+            };
+            chart1.Series.Add(pointSeries);
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double v0 = double.Parse(txtV0.Text.Replace(",", "."),
+                    System.Globalization.CultureInfo.InvariantCulture);
+                double a = double.Parse(txtAcceleration.Text.Replace(",", "."),
+                    System.Globalization.CultureInfo.InvariantCulture);
+                double t = double.Parse(txtTime.Text.Replace(",", "."),
+                    System.Globalization.CultureInfo.InvariantCulture);
+
+                if (t <= 0)
+                {
+                    MessageBox.Show("Время движения должно быть положительным числом!",
+                        "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                CalculateMotion(v0, a, t);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Пожалуйста, введите корректные числовые значения!",
+                    "Ошибка ввода", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка расчета: {ex.Message}",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CalculateMotion(double v0, double a, double t)
+        {
+            double v = v0 + a * t;
+
+            double s = v0 * t + (a * t * t) / 2;
+
+            string motionType = DetermineMotionType(v0, a);
+
+            string description = GenerateDescription(v0, a, t, v, s);
+
+            txtMotionType.Text = motionType;
+            txtDistance.Text = s.ToString("F2");
+            txtFinalSpeed.Text = v.ToString("F2");
+
+            UpdateEquation(v0, a);
+
+            BuildChart(v0, a, t);
+        }
+
+        private string DetermineMotionType(double v0, double a)
+        {
+            if (a == 0)
+            {
+                if (v0 == 0)
+                    return "Покой";
+                else
+                    return "Равномерное движение";
+            }
+            else if (a > 0)
+            {
+                if (v0 == 0)
+                    return "Равноускоренное из состояния покоя";
+                else if (v0 > 0)
+                    return "Равноускоренное движение";
+                else
+                    return "Равноускоренное с началом в противоположную сторону";
+            }
+            else
+            {
+                if (v0 > 0)
+                    return "Равнозамедленное движение";
+                else if (v0 == 0)
+                    return "Невозможно (отрицательное ускорение при нулевой скорости)";
+                else
+                    return "Ускорение в противоположном направлении";
+            }
+        }
+
+        private string GenerateDescription(double v0, double a, double t, double v, double s)
+        {
+            string description = "";
+
+            if (a == 0)
+            {
+                if (v0 == 0)
+                    description = "Тело находится в состоянии покоя. Пройденный путь равен нулю.";
+                else
+                    description = $"Тело движется равномерно со скоростью {Math.Abs(v0):F2} м/с. ";
+                description += $"За время {t:F1} с тело прошло {s:F2} м.";
+            }
+            else if (a > 0)
+            {
+                if (v0 == 0)
+                    description = $"Тело начинает движение из состояния покоя с ускорением {a:F2} м/с². ";
+                else if (v0 > 0)
+                    description = $"Тело движется равноускоренно. Начальная скорость {v0:F2} м/с, ускорение {a:F2} м/с². ";
+                else
+                    description = $"Тело начинает движение в противоположную сторону с ускорением {a:F2} м/с². ";
+
+                description += $"Конечная скорость: {v:F2} м/с. Пройденный путь: {s:F2} м.";
+
+                if (v0 < 0 && v > 0)
+                {
+                    double tStop = -v0 / a;
+                    description += $"\nТело остановилось через {tStop:F2} с и начало движение в обратном направлении.";
+                }
+            }
+            else
+            {
+                description = $"Тело движется равнозамедленно. Начальная скорость {v0:F2} м/с, ускорение {a:F2} м/с². ";
+                description += $"Конечная скорость: {v:F2} м/с. Пройденный путь: {s:F2} м.";
+
+                if (v0 > 0 && v < 0)
+                {
+                    double tStop = -v0 / a;
+                    double sStop = v0 * tStop + (a * tStop * tStop) / 2;
+                    description += $"\nТело остановилось через {tStop:F2} с, пройдя {sStop:F2} м, и начало движение в обратном направлении.";
+                }
+                else if (v0 > 0 && v == 0)
+                {
+                    double tStop = -v0 / a;
+                    double sStop = v0 * tStop + (a * tStop * tStop) / 2;
+                    description += $"\nТело полностью остановилось через {tStop:F2} с, пройдя {sStop:F2} м.";
+                }
+            }
+
+            if (v > 0)
+                description += $"\nВ конечный момент тело движется в положительном направлении.";
+            else if (v < 0)
+                description += $"\nВ конечный момент тело движется в отрицательном направлении.";
+            else
+                description += $"\nВ конечный момент тело остановилось.";
+
+            return description;
+        }
+
+        private void UpdateEquation(double v0, double a)
+        {
+            string sign = a >= 0 ? "+" : "-";
+            double absA = Math.Abs(a);
+
+            if (v0 == 0 && a == 0)
+            {
+                lblEquation.Text = "S(t) = 0";
+            }
+            else if (a == 0)
+            {
+                lblEquation.Text = $"S(t) = {v0:F2}t";
+            }
+            else if (v0 == 0)
+            {
+                lblEquation.Text = $"S(t) = {sign} {absA:F2}t²/2";
+            }
+            else
+            {
+                lblEquation.Text = $"S(t) = {v0:F2}t {sign} {absA:F2}t²/2";
+            }
+        }
+
+        private void BuildChart(double v0, double a, double tMax)
+        {
+            chart1.Series["Path"].Points.Clear();
+            chart1.Series["KeyPoints"].Points.Clear();
+
+            int pointsCount = 100;
+            double step = tMax / pointsCount;
+
+            for (int i = 0; i <= pointsCount; i++)
+            {
+                double time = i * step;
+                double distance = v0 * time + (a * time * time) / 2;
+
+                chart1.Series["Path"].Points.AddXY(time, distance);
+
+                if (i % 10 == 0)
+                {
+                    chart1.Series["KeyPoints"].Points.AddXY(time, distance);
+                }
+            }
+
+            chart1.Series["KeyPoints"].Points.AddXY(0, 0);
+            double finalDistance = v0 * tMax + (a * tMax * tMax) / 2;
+            chart1.Series["KeyPoints"].Points.AddXY(tMax, finalDistance);
+
+            chart1.ChartAreas[0].RecalculateAxesScale();
+
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisY.Minimum = Math.Min(0, chart1.ChartAreas[0].AxisY.Minimum);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtV0.Text = "";
+            txtAcceleration.Text = "";
+            txtTime.Text = "";
+
+            txtMotionType.Text = "";
+            txtDistance.Text = "";
+            txtFinalSpeed.Text = "";
+
+            lblEquation.Text = "S(t) = ";
+
+            chart1.Series["Path"].Points.Clear();
+            chart1.Series["KeyPoints"].Points.Clear();
+
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+            chart1.ChartAreas[0].AxisX.Maximum = 10;
+            chart1.ChartAreas[0].AxisY.Maximum = 10;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblEquation.Text = "S(t) = ";
+        }
+
+    }
+}
