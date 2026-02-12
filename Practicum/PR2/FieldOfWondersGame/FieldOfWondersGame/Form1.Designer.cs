a1576f6333d1eb975da5e23fe8f11faa9c0549c7
Yalovoy <160518237+Yalovoy@users.noreply.github.com>
2026-02-13 00:02:44 +0300

    Add files via upload

---
+++ Practicum/PR2/FieldOfWondersGame/FieldOfWondersGame/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace FieldOfWondersGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxInputWord = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelMixedTitle = new System.Windows.Forms.Label();
+            this.flowLayoutPanelMixed = new System.Windows.Forms.FlowLayoutPanel();
+            this.labelResultTitle = new System.Windows.Forms.Label();
+            this.flowLayoutPanelResult = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonCheck = new System.Windows.Forms.Button();
+            this.buttonNewGame = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.buttonGenerateWord = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Algerian", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(219, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ПОЛЕ ЧУДЕС";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(215, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите слово для угадывания:";
+            // 
+            // textBoxInputWord
+            // 
+            this.textBoxInputWord.Location = new System.Drawing.Point(245, 103);
+            this.textBoxInputWord.Name = "textBoxInputWord";
+            this.textBoxInputWord.Size = new System.Drawing.Size(214, 22);
+            this.textBoxInputWord.TabIndex = 2;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(484, 103);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(127, 26);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "Начать игру";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // labelMixedTitle
+            // 
+            this.labelMixedTitle.AutoSize = true;
+            this.labelMixedTitle.Location = new System.Drawing.Point(9, 156);
+            this.labelMixedTitle.Name = "labelMixedTitle";
+            this.labelMixedTitle.Size = new System.Drawing.Size(151, 16);
+            this.labelMixedTitle.TabIndex = 4;
+            this.labelMixedTitle.Text = "Перепутанные буквы:";
+            this.labelMixedTitle.Visible = false;
+            // 
+            // flowLayoutPanelMixed
+            // 
+            this.flowLayoutPanelMixed.Location = new System.Drawing.Point(9, 198);
+            this.flowLayoutPanelMixed.Name = "flowLayoutPanelMixed";
+            this.flowLayoutPanelMixed.Size = new System.Drawing.Size(739, 70);
+            this.flowLayoutPanelMixed.TabIndex = 5;
+            // 
+            // labelResultTitle
+            // 
+            this.labelResultTitle.AutoSize = true;
+            this.labelResultTitle.Location = new System.Drawing.Point(9, 282);
+            this.labelResultTitle.Name = "labelResultTitle";
+            this.labelResultTitle.Size = new System.Drawing.Size(161, 16);
+            this.labelResultTitle.TabIndex = 6;
+            this.labelResultTitle.Text = "Собрать буквы в слово:";
+            this.labelResultTitle.Visible = false;
+            // 
+            // flowLayoutPanelResult
+            // 
+            this.flowLayoutPanelResult.Location = new System.Drawing.Point(9, 320);
+            this.flowLayoutPanelResult.Name = "flowLayoutPanelResult";
+            this.flowLayoutPanelResult.Size = new System.Drawing.Size(739, 56);
+            this.flowLayoutPanelResult.TabIndex = 6;
+            // 
+            // buttonCheck
+            // 
+            this.buttonCheck.Location = new System.Drawing.Point(44, 403);
+            this.buttonCheck.Name = "buttonCheck";
+            this.buttonCheck.Size = new System.Drawing.Size(126, 35);
+            this.buttonCheck.TabIndex = 7;
+            this.buttonCheck.Text = "Проверить";
+            this.buttonCheck.UseVisualStyleBackColor = true;
+            this.buttonCheck.Click += new System.EventHandler(this.ButtonCheck_Click);
+            // 
+            // buttonNewGame
+            // 
+            this.buttonNewGame.Location = new System.Drawing.Point(485, 403);
+            this.buttonNewGame.Name = "buttonNewGame";
+            this.buttonNewGame.Size = new System.Drawing.Size(126, 35);
+            this.buttonNewGame.TabIndex = 8;
+            this.buttonNewGame.Text = "Заново";
+            this.buttonNewGame.UseVisualStyleBackColor = true;
+            this.buttonNewGame.Click += new System.EventHandler(this.ButtonNewGame_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(75, 467);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 16);
+            this.labelResult.TabIndex = 10;
+            // 
+            // buttonGenerateWord
+            // 
+            this.buttonGenerateWord.Location = new System.Drawing.Point(485, 148);
+            this.buttonGenerateWord.Name = "buttonGenerateWord";
+            this.buttonGenerateWord.Size = new System.Drawing.Size(126, 23);
+            this.buttonGenerateWord.TabIndex = 11;
+            this.buttonGenerateWord.Text = "Генерировать";
+            this.buttonGenerateWord.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 713);
+            this.Controls.Add(this.buttonGenerateWord);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonNewGame);
+            this.Controls.Add(this.buttonCheck);
+            this.Controls.Add(this.flowLayoutPanelResult);
+            this.Controls.Add(this.labelResultTitle);
+            this.Controls.Add(this.flowLayoutPanelMixed);
+            this.Controls.Add(this.labelMixedTitle);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxInputWord);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxInputWord;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelMixedTitle;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelMixed;
+        private System.Windows.Forms.Label labelResultTitle;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelResult;
+        private System.Windows.Forms.Button buttonCheck;
+        private System.Windows.Forms.Button buttonNewGame;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button buttonGenerateWord;
+    }
+}
+
---
+++ Practicum/PR2/FieldOfWondersGame/FieldOfWondersGame/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FieldOfWondersGame
+{
+    public partial class Form1 : Form
+    {
+        private string originalWord = "";
+        private List<char> shuffledLetters = new List<char>();
+        private List<Button> mixedLetterButtons = new List<Button>();
+        private List<Button> resultLetterSlots = new List<Button>();
+        private int attempts = 0;
+        private bool gameStarted = false;
+
+        private readonly List<string> dictionary = new List<string>
+        {
+            "ПРОГРАММА", "КОМПЬЮТЕР", "ИНТЕРНЕТ", "ВИЗУАЛИЗАЦИЯ",
+            "ФОРМА", "КЛАВИАТУРА", "МЫШЬ", "ОКНО"
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            this.Text = "Поле чудес";
+            this.Size = new Size(500, 500);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackColor = Color.White;
+
+            buttonStart.Click += ButtonStart_Click;
+            buttonCheck.Click += ButtonCheck_Click;
+            buttonNewGame.Click += ButtonNewGame_Click;
+            buttonGenerateWord.Click += ButtonGenerateWord_Click;
+        }
+
+        private void ButtonGenerateWord_Click(object sender, EventArgs e)
+        {
+            if (!gameStarted)
+            {
+                GenerateAndSetWord();
+            }
+            else
+            {
+                var confirmResult = MessageBox.Show(
+                    "Игра уже идет. Начать новую игру с новым словом?",
+                    "Подтверждение",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning);
+
+                if (confirmResult == DialogResult.Yes)
+                {
+                    ResetGame();
+                    GenerateAndSetWord();
+                }
+            }
+        }
+
+        private void GenerateAndSetWord()
+        {
+            Random random = new Random();
+            int index = random.Next(dictionary.Count);
+            string randomWord = dictionary[index];
+
+            textBoxInputWord.Text = randomWord;
+
+            StartNewGame(randomWord);
+        }
+
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            string inputWord = textBoxInputWord.Text.Trim().ToUpper();
+
+            if (string.IsNullOrWhiteSpace(inputWord))
+            {
+                MessageBox.Show("Введите слово для начала игры или нажмите 'Генерировать слово'!");
+                return;
+            }
+
+            if (inputWord.Length < 3)
+            {
+                MessageBox.Show("Слово должно содержать минимум 3 буквы!");
+                return;
+            }
+
+            if (!inputWord.All(char.IsLetter))
+            {
+                MessageBox.Show("Слово должно содержать только буквы!");
+                return;
+            }
+
+            StartNewGame(inputWord);
+        }
+
+        private void StartNewGame(string word)
+        {
+            originalWord = word;
+            shuffledLetters = ShuffleLetters(word.ToList());
+            attempts = 0;
+            gameStarted = true;
+
+            SetupMixedLettersPanel();
+            SetupResultPanel();
+
+            ShowGameElements();
+
+            labelResult.Text = "";
+            labelResult.Visible = false;
+        }
+
+        private List<char> ShuffleLetters(List<char> letters)
+        {
+            Random random = new Random();
+            List<char> shuffled = new List<char>(letters);
+
+            for (int i = shuffled.Count - 1; i > 0; i--)
+            {
+                int j = random.Next(i + 1);
+                char temp = shuffled[i];
+                shuffled[i] = shuffled[j];
+                shuffled[j] = temp;
+            }
+
+            return shuffled;
+        }
+
+        private void SetupMixedLettersPanel()
+        {
+            flowLayoutPanelMixed.Controls.Clear();
+            mixedLetterButtons.Clear();
+
+            foreach (char letter in shuffledLetters)
+            {
+                Button letterButton = new Button
+                {
+                    Text = letter.ToString(),
+                    Size = new Size(40, 40),
+                    Font = new Font("Arial", 14, FontStyle.Bold),
+                    BackColor = Color.LightBlue,
+                    ForeColor = Color.Black,
+                    Tag = letter,
+                    Margin = new Padding(5),
+                    Cursor = Cursors.Hand
+                };
+
+                letterButton.Click += MixedLetterButton_Click;
+                mixedLetterButtons.Add(letterButton);
+                flowLayoutPanelMixed.Controls.Add(letterButton);
+            }
+        }
+
+        private void SetupResultPanel()
+        {
+            flowLayoutPanelResult.Controls.Clear();
+            resultLetterSlots.Clear();
+
+            for (int i = 0; i < originalWord.Length; i++)
+            {
+                Button slotButton = new Button
+                {
+                    Text = "",
+                    Size = new Size(40, 40),
+                    Font = new Font("Arial", 14, FontStyle.Bold),
+                    BackColor = Color.White,
+                    ForeColor = Color.Black,
+                    Tag = -1,
+                    Margin = new Padding(5),
+                    Cursor = Cursors.Hand
+                };
+
+                slotButton.Click += ResultSlotButton_Click;
+                resultLetterSlots.Add(slotButton);
+                flowLayoutPanelResult.Controls.Add(slotButton);
+            }
+        }
+
+        private void MixedLetterButton_Click(object sender, EventArgs e)
+        {
+            if (!gameStarted) return;
+
+            Button clickedButton = sender as Button;
+            if (clickedButton == null) return;
+
+            char letter = (char)clickedButton.Tag;
+            int buttonIndex = mixedLetterButtons.IndexOf(clickedButton);
+
+            for (int i = 0; i < resultLetterSlots.Count; i++)
+            {
+                if (string.IsNullOrEmpty(resultLetterSlots[i].Text))
+                {
+                    resultLetterSlots[i].Text = letter.ToString();
+                    resultLetterSlots[i].Tag = buttonIndex;
+                    resultLetterSlots[i].BackColor = Color.LightGreen;
+                    break;
+                }
+            }
+
+            UpdateGameState();
+        }
+
+        private void ResultSlotButton_Click(object sender, EventArgs e)
+        {
+            if (!gameStarted) return;
+
+            Button clickedSlot = sender as Button;
+            if (clickedSlot == null || string.IsNullOrEmpty(clickedSlot.Text)) return;
+
+            int buttonIndex = (int)clickedSlot.Tag;
+            if (buttonIndex >= 0 && buttonIndex < mixedLetterButtons.Count)
+            {
+                mixedLetterButtons[buttonIndex].Enabled = true;
+                mixedLetterButtons[buttonIndex].BackColor = Color.LightBlue;
+            }
+
+            clickedSlot.Text = "";
+            clickedSlot.Tag = -1;
+            clickedSlot.BackColor = Color.White;
+
+            UpdateGameState();
+        }
+
+        private void ButtonCheck_Click(object sender, EventArgs e)
+        {
+            if (!gameStarted) return;
+
+            attempts++;
+
+            string userWord = "";
+            foreach (Button slot in resultLetterSlots)
+            {
+                userWord += slot.Text;
+            }
+
+            if (userWord == originalWord)
+            {
+                labelResult.Text = "Правильно!";
+                labelResult.ForeColor = Color.Green;
+                labelResult.Font = new Font("Arial", 12, FontStyle.Bold);
+
+                foreach (Button slot in resultLetterSlots)
+                {
+                    slot.BackColor = Color.Green;
+                    slot.ForeColor = Color.White;
+                }
+
+                foreach (Button mixedButton in mixedLetterButtons)
+                {
+                    mixedButton.Enabled = false;
+                    mixedButton.BackColor = Color.LightGray;
+                }
+
+                buttonCheck.Enabled = false;
+            }
+            else
+            {
+                labelResult.Text = $"Неверно! Правильное слово: {originalWord}";
+                labelResult.ForeColor = Color.Red;
+                labelResult.Font = new Font("Arial", 12, FontStyle.Bold);
+
+                for (int i = 0; i < resultLetterSlots.Count; i++)
+                {
+                    if (i < userWord.Length && i < originalWord.Length)
+                    {
+                        if (userWord[i] == originalWord[i])
+                        {
+                            resultLetterSlots[i].BackColor = Color.Green;
+                        }
+                        else
+                        {
+                            resultLetterSlots[i].BackColor = Color.Red;
+                        }
+                    }
+                }
+            }
+
+            labelResult.Visible = true;
+        }
+
+        private void ButtonNewGame_Click(object sender, EventArgs e)
+        {
+            ResetGame();
+            textBoxInputWord.Text = "";
+            textBoxInputWord.Focus();
+        }
+
+        private void ResetGame()
+        {
+            originalWord = "";
+            shuffledLetters.Clear();
+            mixedLetterButtons.Clear();
+            resultLetterSlots.Clear();
+            attempts = 0;
+            gameStarted = false;
+
+            flowLayoutPanelMixed.Controls.Clear();
+            flowLayoutPanelResult.Controls.Clear();
+
+            HideGameElements();
+
+            labelResult.Text = "";
+            labelResult.Visible = false;
+        }
+
+        private void ShowGameElements()
+        {
+            labelMixedTitle.Visible = true;
+            flowLayoutPanelMixed.Visible = true;
+            labelResultTitle.Visible = true;
+            flowLayoutPanelResult.Visible = true;
+            buttonCheck.Visible = true;
+            buttonNewGame.Visible = true;
+
+            buttonStart.Enabled = false;
+            textBoxInputWord.Enabled = false;
+        }
+
+        private void HideGameElements()
+        {
+            labelMixedTitle.Visible = false;
+            flowLayoutPanelMixed.Visible = false;
+            labelResultTitle.Visible = false;
+            flowLayoutPanelResult.Visible = false;
+            buttonCheck.Visible = false;
+            buttonNewGame.Visible = false;
+
+            buttonStart.Enabled = true;
+            textBoxInputWord.Enabled = true;
+        }
+
+        private void UpdateGameState()
+        {
+            bool allSlotsFilled = resultLetterSlots.All(slot => !string.IsNullOrEmpty(slot.Text));
+            buttonCheck.Enabled = allSlotsFilled;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
