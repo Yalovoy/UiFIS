a1d7e7f8bf5d09fdb120e3f310f0feca100d8ded
Yalovoy <160518237+Yalovoy@users.noreply.github.com>
2026-02-13 00:44:07 +0300

    Add files via upload

---
+++ PracticalWork/PR3/ReliabilityCalculator2/ReliabilityCalculator2/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿ namespace ReliabilityCalculatorV7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxT = new System.Windows.Forms.MaskedTextBox();
+            this.textBoxVx = new System.Windows.Forms.MaskedTextBox();
+            this.textBoxMt = new System.Windows.Forms.MaskedTextBox();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBoxResult = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxT
+            // 
+            this.textBoxT.Location = new System.Drawing.Point(293, 211);
+            this.textBoxT.Name = "textBoxT";
+            this.textBoxT.Size = new System.Drawing.Size(100, 22);
+            this.textBoxT.TabIndex = 25;
+            // 
+            // textBoxVx
+            // 
+            this.textBoxVx.Location = new System.Drawing.Point(293, 139);
+            this.textBoxVx.Name = "textBoxVx";
+            this.textBoxVx.Size = new System.Drawing.Size(100, 22);
+            this.textBoxVx.TabIndex = 24;
+            // 
+            // textBoxMt
+            // 
+            this.textBoxMt.Location = new System.Drawing.Point(293, 75);
+            this.textBoxMt.Name = "textBoxMt";
+            this.textBoxMt.Size = new System.Drawing.Size(100, 22);
+            this.textBoxMt.TabIndex = 23;
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Location = new System.Drawing.Point(293, 21);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(100, 22);
+            this.textBoxN.TabIndex = 22;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(216, 253);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(109, 23);
+            this.btnCalculate.TabIndex = 21;
+            this.btnCalculate.Text = "Рассчитать";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(402, 145);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(14, 16);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "v";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(399, 214);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 16);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "t, час";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 16);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Количество изделий на испытаниях:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(35, 214);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(257, 16);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Наработка для расчета отказа (часы):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(399, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "N";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(199, 16);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Коэффициент вариации (0-1):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(241, 16);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Средняя наработка на отказ (часы):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(399, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 16);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Mt, ч";
+            // 
+            // richTextBoxResult
+            // 
+            this.richTextBoxResult.Location = new System.Drawing.Point(12, 282);
+            this.richTextBoxResult.Name = "richTextBoxResult";
+            this.richTextBoxResult.Size = new System.Drawing.Size(561, 226);
+            this.richTextBoxResult.TabIndex = 26;
+            this.richTextBoxResult.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 519);
+            this.Controls.Add(this.richTextBoxResult);
+            this.Controls.Add(this.textBoxT);
+            this.Controls.Add(this.textBoxVx);
+            this.Controls.Add(this.textBoxMt);
+            this.Controls.Add(this.textBoxN);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox textBoxT;
+        private System.Windows.Forms.MaskedTextBox textBoxVx;
+        private System.Windows.Forms.MaskedTextBox textBoxMt;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBoxResult;
+    }
+}
+
---
+++ PracticalWork/PR3/ReliabilityCalculator2/ReliabilityCalculator2/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ReliabilityCalculatorV7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetupForm();
+        }
+
+        private void SetupForm()
+        {
+            richTextBoxResult.Font = new Font("Consolas", 10);
+            richTextBoxResult.ReadOnly = true;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double N = double.Parse(textBoxN.Text.Replace(",", "."),
+                    System.Globalization.CultureInfo.InvariantCulture);
+                double Mt = double.Parse(textBoxMt.Text.Replace(",", "."),
+                    System.Globalization.CultureInfo.InvariantCulture);
+                double v = double.Parse(textBoxVx.Text.Replace(",", "."),
+                    System.Globalization.CultureInfo.InvariantCulture);
+                double t = double.Parse(textBoxT.Text.Replace(",", "."),
+                    System.Globalization.CultureInfo.InvariantCulture);
+
+                double sigma = v * Mt;
+
+                double Up = (t - Mt) / sigma;
+
+                double P = NormalCDF(Up);
+
+                double Q = P;
+
+                double failedCount = Q * N;
+
+                string result = $"РАСЧЕТ ПОКАЗАТЕЛЕЙ НАДЕЖНОСТИ\n";
+                result += $"================================\n\n";
+                result += $"ИСХОДНЫЕ ДАННЫЕ:\n";
+                result += $"Количество изделий (N): {N} шт.\n";
+                result += $"Средняя наработка (Mt): {Mt} ч\n";
+                result += $"Коэффициент вариации (ν): {v}\n";
+                result += $"Наработка для расчета (t): {t} ч\n\n";
+
+                result += $"РЕЗУЛЬТАТЫ РАСЧЕТА:\n";
+                result += $"================================\n\n";
+
+                result += $"1. Среднее квадратическое отклонение:\n";
+                result += $"   σ = ν × Mt = {v} × {Mt} = {sigma:F1} час\n\n";
+
+                result += $"2. Квантиль нормированного нормального распределения:\n";
+                result += $"   U_p = (t - Mt) / σ = ({t} - {Mt}) / {sigma:F1} = {Up:F4}\n\n";
+
+                result += $"3. Функция Лапласа (нормированная функция распределения):\n";
+                result += $"   Φ(U_p) = Φ({Up:F4}) = {P:F4}\n\n";
+
+                result += $"4. Вероятность отказа:\n";
+                result += $"   Q(t) = Φ(U_p) = {Q:F4} или {Q * 100:F2}%\n\n";
+
+                result += $"5. Количество отказавших изделий:\n";
+                result += $"   n(t) = Q(t) × N = {Q:F4} × {N} = {failedCount:F1} из {N}\n\n";
+
+                result += $"================================\n";
+                result += $"Расчет выполнен для нормального распределения.\n";
+
+                richTextBoxResult.Text = result;
+
+                CalculateForInterval(N, Mt, sigma);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Пожалуйста, введите корректные числовые значения.",
+                    "Ошибка ввода", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка расчета: {ex.Message}",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CalculateForInterval(double N, double Mt, double sigma)
+        {
+            try
+            {
+                double t_start = 200;
+                double t_end = 250;
+
+                double Up_start = (t_start - Mt) / sigma;
+                double Up_end = (t_end - Mt) / sigma;
+
+                double Q_start = NormalCDF(Up_start);
+                double Q_end = NormalCDF(Up_end);
+
+                double Q_interval = Q_end - Q_start;
+                double failedInInterval = Q_interval * N;
+
+                string intervalInfo = $"\n\nДОПОЛНИТЕЛЬНЫЙ РАСЧЕТ ДЛЯ ИНТЕРВАЛА [200, 250] ч:\n";
+                intervalInfo += $"================================================\n\n";
+
+                intervalInfo += $"1. Для tₐ = {t_start} часов:\n";
+                intervalInfo += $"   U_p(tₐ) = (tₐ - M_t) / σ = ({t_start} - {Mt}) / {sigma:F1} = {Up_start:F4}\n";
+                intervalInfo += $"   Φ(U_p(tₐ)) = Φ({Up_start:F4}) = {Q_start:F4}\n\n";
+
+                intervalInfo += $"2. Для tᵦ = {t_end} часов:\n";
+                intervalInfo += $"   U_p(tᵦ) = (tᵦ - M_t) / σ = ({t_end} - {Mt}) / {sigma:F1} = {Up_end:F4}\n";
+                intervalInfo += $"   Φ(U_p(tᵦ)) = Φ({Up_end:F4}) = {Q_end:F4}\n\n";
+
+                intervalInfo += $"3. Вероятность отказа в интервале:\n";
+                intervalInfo += $"   ΔQ = Q(tᵦ) - Q(tₐ) = Φ(U_p(tᵦ)) - Φ(U_p(tₐ))\n";
+                intervalInfo += $"   ΔQ = {Q_end:F4} - {Q_start:F4} = {Q_interval:F4}\n\n";
+
+                intervalInfo += $"4. Количество отказавших в интервале:\n";
+                intervalInfo += $"   Δn = ΔQ × N = {Q_interval:F4} × {N} = {failedInInterval:F2} долот\n\n";
+
+                intervalInfo += $"================================================\n";
+                intervalInfo += $"Ответ: {failedInInterval:F2} долот";
+
+                richTextBoxResult.AppendText(intervalInfo);
+            }
+            catch (Exception ex)
+            {
+                richTextBoxResult.AppendText($"\n\nОшибка при расчете интервала: {ex.Message}");
+            }
+        }
+
+        private double NormalCDF(double x)
+        {
+            double a1 = 0.254829592;
+            double a2 = -0.284496736;
+            double a3 = 1.421413741;
+            double a4 = -1.453152027;
+            double a5 = 1.061405429;
+            double p = 0.3275911;
+
+            int sign = 1;
+            if (x < 0)
+                sign = -1;
+            x = Math.Abs(x) / Math.Sqrt(2.0);
+
+            double t = 1.0 / (1.0 + p * x);
+            double y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * Math.Exp(-x * x);
+
+            return 0.5 * (1.0 + sign * y);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
